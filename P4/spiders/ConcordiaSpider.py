55c5ce9308a831686eebe7e3a77207a0bcd7c25f
BlackSound1 <matthewr@videotron.ca>
2023-11-15 12:16:21 -0500

    Remove a log

--- P4/spiders/ConcordiaSpider.py
+++ P4/spiders/ConcordiaSpider.py
@@ -12,7 +12,6 @@ class MySpider(scrapy.Spider):
         settings.set('ROBOTSTXT_OBEY', 'True', priority='spider')
 
     def parse(self, response, **kwargs):
-        self.log(f"SEX: {response.url}")
         page = response.url.split('//')[-1]
         filename = f'html-files/{page}.html'
         Path('html-files').mkdir(exist_ok=True, parents=True)
