0430298ec991a1cdcb736ca4813761f83b879b0a
BlackSound1 <matthewr@videotron.ca>
2023-11-15 18:53:54 -0500

    Formatting and documentation

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 from scrapy.crawler import CrawlerProcess
+from twisted.internet import reactor, defer
+
 from P4.spiders.ConcordiaSpider import MainSpider
 from P4.spiders.LinkSpider import LinkSpider
-from twisted.internet import reactor, defer
 
 
 @defer.inlineCallbacks
 def crawl(*processes: CrawlerProcess):
+    """
+    A callback generator function to define how the CrawlerProcesses are called.
+
+    :param processes: 1 or more CrawlerProcesses. Must have a concordia process and link process,
+                      but allows for extensibility.
+    :return: The results of each spider crawl
+    """
+
+    # Crawl with the concordia spider
     yield processes[0].crawl(MainSpider)
 
+    # Get all the links to traverse the links spider with
     with open('text_files/concordia_links.txt', 'rt') as f:
         links = [l.strip('\n') for l in f.readlines()]
 
+    # Crawl with the links spider, given the links to crawl
     yield processes[1].crawl(LinkSpider, start_urls=links)
 
+    # End the process once finished
     reactor.stop()
 
 
 def main():
+    # Create 2 processes, 1 for each spider
     concordia_process = CrawlerProcess()
     links_process = CrawlerProcess()
 
+    # Call the crawl callback function
     crawl(concordia_process, links_process)
 
+    # Actually run the twisted reactor
     reactor.run()
 
 
