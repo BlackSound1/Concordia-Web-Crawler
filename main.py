e404a2e18c4d7d4371aa4facad2754557df4baad
BlackSound1 <matthewr@videotron.ca>
2023-11-15 18:48:05 -0500

    Both Spiders work sequentially

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
-from scrapy.crawler import CrawlerProcess, CrawlerRunner
+from scrapy.crawler import CrawlerProcess
 from P4.spiders.ConcordiaSpider import MainSpider
 from P4.spiders.LinkSpider import LinkSpider
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
 
 
-def main():
-    process = CrawlerProcess()
-
-    # process.crawl(MainSpider)
-    # process.start()
+@defer.inlineCallbacks
+def crawl(*processes: CrawlerProcess):
+    yield processes[0].crawl(MainSpider)
 
     with open('text_files/concordia_links.txt', 'rt') as f:
         links = [l.strip('\n') for l in f.readlines()]
 
-    # print(links)
-    process.crawl(LinkSpider, start_urls=links)
-    process.start()
+    yield processes[1].crawl(LinkSpider, start_urls=links)
+
+    reactor.stop()
+
+
+def main():
+    concordia_process = CrawlerProcess()
+    links_process = CrawlerProcess()
+
+    crawl(concordia_process, links_process)
+
+    reactor.run()
 
 
 if __name__ == '__main__':
