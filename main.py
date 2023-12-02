037ed0e1cd215430a1189867965dccb23e2b50dc
BlackSound1 <matthewr@videotron.ca>
2023-12-01 21:59:38 -0500

    Added argparse to let user optionally decide how many files to save

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from pathlib import Path
 
 from scrapy.crawler import CrawlerProcess
+from argparse import ArgumentParser
 
 from P4.spiders.MainSpider import MainSpider
 
 
+# Create an argument parser to let user decide how many files to process
+parser = ArgumentParser(description="Concordia Scraper")
+parser.add_argument('--num_files', metavar='n', type=int,
+                    help="The number of files to process", default=10, required=False)
+
+
 def main():
 
+    # Parse the command-line arguments passed to this script, if any
+    args = parser.parse_args()
+
+    # Clear the file folder(s)
     clear_folder()
 
     # Create and run a CrawlerProcess based on the spider
     process = CrawlerProcess()
-    process.crawl(MainSpider, max_files=10)
+    process.crawl(MainSpider, max_files=args.num_files)
     process.start()
 
 
