cf75a66ccf5b096f160af9515419a06eefd0e8f9
BlackSound1 <matthewr@videotron.ca>
2026-05-16 13:53:43 -0400

    Add Ruff

--- crawl.py
+++ crawl.py
@@ -15,19 +15,17 @@ def main():
     # Parse the command-line arguments passed to this script, if any
     args = parser.parse_args()
 
-    # Clear the file folder(s)
-    _clear_folder()
+    _clear_folders()
 
     print("\n--- Web Crawling ---\n")
 
     # Create and run a CrawlerProcess based on the spider. Inspired by: https://stackoverflow.com/a/31374345
-    # Using Scrapy version 2.8.0 https://github.com/scrapy/scrapy
     process = CrawlerProcess()
     process.crawl(MainSpider, max_files=args.num_files)
     process.start()
 
 
-def _clear_folder():
+def _clear_folders():
     """
     Delete the contents of necessary folders when starting the app.
     """
